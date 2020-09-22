256af1715ae5845490295c7d4286276e429d2170
ayayacc <ayaya_cc@163.com>
2020-09-22 10:36:58 +0800

    取消事件

---
+++ src/main/java/com/kl/parkLine/util/Const.java
@@ -0,0 +1,9 @@
+package com.kl.parkLine.util;
+
+public class Const
+{
+    //信路通返回值
+    public final static Integer CLT_RET_CODE_OK = 0; //成功
+    public final static Integer CLT_RET_CODE_FAILED = 3; //执行失败
+    public final static Integer CLT_RET_CODE_INVALID_PARAM = 4; //无效参数
+}
